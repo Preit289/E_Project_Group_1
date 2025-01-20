7436694625a2807f47493c7a1b6c1d264144fcd1
Dangvinh77 <phamdangvinh2002@gmail.com>
2025-01-21 04:54:22 +0700

    Update Notfound page

--- src/pages/Notfound.jsx
+++ src/pages/Notfound.jsx
@@ -1,9 +1,14 @@
+import { useNavigate } from "react-router-dom";
 const Notfound = () => {
+  const navigate = useNavigate();
+  const returnHomePage = () => {
+    navigate("/");
+  };
   return (
     <>
       <h1>404 Error</h1>
       <h4>This page could not be found </h4>
-      <button>Homepage</button>
+      <button onClick={returnHomePage}>Homepage</button>
     </>
   );
 };
