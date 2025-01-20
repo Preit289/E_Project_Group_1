ab3933f6fc372ae7bc850d3e21d6f7d6dac01bf6
Dangvinh77 <phamdangvinh2002@gmail.com>
2025-01-21 04:26:22 +0700

    Update demo Layout

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -4,28 +4,30 @@ const Layout = () => {
     <>
       <header>
         <div className="top-Navbar">
-          <div className="info-contact"></div>
-          <div className="flag-lang"></div>
-          <div className="social-icon"></div>
+          <div className="info-contact">Info Contact</div>
+          <div className="flag-lang">Lang</div>
+          <div className="social-icon">Social</div>
           <div>Visitor Count Area</div>
         </div>
         <nav className="Navbar">
           <Link to="/">Svg of Homepage</Link>
-          <Link to="/" className="logo"></Link>
-          <Link to="/"></Link>
-          <Link to="/"></Link>
-          <Link to="/"></Link>
-          <Link to="/"></Link>
-          <Link to="/"></Link>
+          <Link to="/" className="logo">
+            Logo
+          </Link>
+          <Link to="/">Home</Link>
+          <Link to="/">About Us</Link>
+          <Link to="/">Gallery</Link>
+          <Link to="/">Activity</Link>
+          <Link to="/">Contact Us</Link>
         </nav>
       </header>
       <section>
         <Outlet />
       </section>
       <footer className="Footer">
-        <div className="mail-contact"></div>
-        <div className="nav-contact"></div>
-        <div className="bot-Footer"></div>
+        <div className="mail-contact">Contact me by email here</div>
+        <div className="nav-contact">Navigations footer</div>
+        <div className="bot-Footer">Copyright</div>
       </footer>
     </>
   );
