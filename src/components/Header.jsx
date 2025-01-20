3748bb842360985c14f792482588c743eaa93b3e
Dangvinh77 <phamdangvinh2002@gmail.com>
2025-01-21 05:44:25 +0700

    Update Header components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,32 @@
+import React, { useState, useLayoutEffect, useRef, useCallback } from "react";
 import "../styles/components/Header.scss";
 import { Link } from "react-router-dom";
 const Header = () => {
+  const [visitCount, setVisitCount] = useState(0);
+  const isInitialMount = useRef(true); // Biến cờ để kiểm soát lần render đầu tiên
+  // Load visit count from localStorage and increment it
+  useLayoutEffect(() => {
+    if (isInitialMount.current) {
+      // Chỉ chạy logic này ở lần render đầu tiên
+      const storedVisitCount = localStorage.getItem("page_view");
+      if (storedVisitCount) {
+        const newCount = Number(storedVisitCount) + 1; // Tăng giá trị lên 1
+        setVisitCount(newCount);
+        localStorage.setItem("page_view", newCount);
+      } else {
+        setVisitCount(1); // Nếu không có giá trị, đặt thành 1
+        localStorage.setItem("page_view", 1);
+      }
+      isInitialMount.current = false; // Đánh dấu đã render lần đầu
+    }
+  }, []);
+
+  // Reset visit count to 0
+  const handleReset = useCallback(() => {
+    localStorage.setItem("page_view", 0); // Đặt lại giá trị trong localStorage thành 0
+    setVisitCount(0); // Đặt lại visitCount thành 0
+  }, []);
+
   return (
     <>
       <div className="Header">
@@ -22,6 +48,13 @@ const Header = () => {
           <Link to="/tickets">Tickets</Link>
           <Link to="/contactus">Contact Us</Link>
         </nav>
+        <div className="visitor-count">
+          <div>Visitor count:</div>
+          <div className="website-counter">{visitCount}</div>
+          <button id="reset" onClick={handleReset}>
+            Reset
+          </button>
+        </div>
       </div>
     </>
   );
