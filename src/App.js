34e75f65b67cfca2015dc26e89f895fe5e7338be
Dangvinh77 <phamdangvinh2002@gmail.com>
2025-01-20 17:39:19 +0700

    Update

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import CreateCRUD from './components/CreateCRUD';
 import ReadCRUD from './components/ReadCRUD';
 import UpdateCRUD from './components/UpdateCRUD';
-import NoPage from './components/NoPage'
+import Notfound from './pages/Notfound';
 import Layout from './layout/Layout';
 import "./App.scss"
 const App = () => {
@@ -14,7 +14,7 @@ const App = () => {
             <Route path="/" element={<ReadCRUD />} />
             <Route path="/create" element={<CreateCRUD />} />
             <Route path="/update" element={<UpdateCRUD />} />
-            <Route path="*" element={<NoPage />} />
+            <Route path="*" element={<Notfound />} />
           </Route>
         </Routes>
       </BrowserRouter>
---
+++ src/pages/Notfound.jsx
@@ -0,0 +1,11 @@
+const Notfound = () => {
+  return (
+    <>
+      <h1>404 Error</h1>
+      <h4>This page could not be found </h4>
+      <button>Homepage</button>
+    </>
+  );
+};
+
+export default Notfound;
