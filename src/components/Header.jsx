16d6430c1bf210c1268f23ca07ede017285d3dfb
Dangvinh77 <phamdangvinh2002@gmail.com>
2025-01-28 05:02:59 +0700

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,10 @@
-import React, { useState, useLayoutEffect, useRef, useCallback } from "react";
+import React, {
+  useState,
+  useEffect,
+  useLayoutEffect,
+  useRef,
+  useCallback,
+} from "react";
 import "../styles/components/Header.scss";
 import "../../node_modules/boxicons/css/boxicons.min.css";
 import logo from "../assets/img/keansburg-logo.png";
@@ -6,6 +12,8 @@ import { Link } from "react-router-dom";
 const Header = () => {
   const [visitCount, setVisitCount] = useState(0);
   const isInitialMount = useRef(true); // Biến cờ để kiểm soát lần render đầu tiên
+  const [isTopNavbarHidden, setIsTopNavbarHidden] = useState(false);
+  const [isNavbarFixed, setIsNavbarFixed] = useState(false);
   // Load visit count from localStorage and increment it
   useLayoutEffect(() => {
     if (isInitialMount.current) {
@@ -28,11 +36,30 @@ const Header = () => {
     localStorage.setItem("page_view", 0); // Đặt lại giá trị trong localStorage thành 0
     setVisitCount(0); // Đặt lại visitCount thành 0
   }, []);
+  useEffect(() => {
+    const handleScroll = () => {
+      // Remember measure height of top-Navbar again when edit Styles {100: 100px}
+      if (window.scrollY > 53) {
+        // Cuộn quá 100px thì ẩn
+        setIsTopNavbarHidden(true);
+        setIsNavbarFixed(true);
+      } else {
+        setIsTopNavbarHidden(false);
+        setIsNavbarFixed(false);
+      }
+    };
 
+    window.addEventListener("scroll", handleScroll);
+
+    // Cleanup khi component unmount để tránh memory leaks
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
   return (
     <>
       <div className="Header">
-        <div className="top-Navbar">
+        <div className={`top-Navbar ${isTopNavbarHidden ? "hidden" : ""}`}>
           <div className="info-contact">Info Contact</div>
           <div className="social-icon">Social</div>
           <div className="SearchArea">Search icon</div>
@@ -57,7 +84,7 @@ const Header = () => {
             </button>
           </div>
         </div>
-        <nav className="Navbar">
+        <nav className={`Navbar ${isNavbarFixed ? "fixed" : ""}`}>
           <Link to="/">
             <i className="bx bxs-home"></i>
           </Link>
