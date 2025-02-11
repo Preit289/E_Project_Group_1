198e40a263f72048d77decd7b0deb925f5fad1ad
Preit289 <hoanthinh.david@gmail.com>
2025-02-11 07:03:14 +0700

    activities

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,31 +1,3 @@
-// import { useEffect } from "react";
-// import "../styles/pages/HomePage.scss";
-// const HomePage = () => {
-//   // Setup title for page
-//   useEffect(() => {
-//     document.title = "Homepage | KEANSBURG PARK";
-//   }, []);
-//   return (
-//     <>
-//       <h1>Home</h1>
-//       <p
-//         style={{
-//           fontSize: "50px",
-//           padding: "0px",
-//           textAlign: "justify",
-//         }}
-//       >
-//         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod
-//         tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor
-//         sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt
-//         ut labore et dolore magna aliqua.
-//       </p>
-//     </>
-//   );
-// };
-
-// export default HomePage;
-
 import React from "react";
 import { useEffect } from "react";
 import "../styles/pages/HomePage.scss";
@@ -39,6 +11,13 @@ import game from "../assets/img/game.png";
 import beach from "../assets/img/beach.jpg";
 import cage from "../assets/img/battingCage.jpg";
 import fishing from "../assets/img/fishing.jpeg";
+import lagoon from "../assets/img/lagoon.png";
+import tub from "../assets/img/hottub.jpg";
+import slides from "../assets/img/slides.jpg";
+import lockers from "../assets/img/lockers.png";
+import dining from "../assets/img/dining.png";
+import changing from "../assets/img/changingRoom.jpg";
+
 
 const Homepage = () => {
   useEffect(() => {
@@ -60,7 +39,7 @@ const Homepage = () => {
             </video>
             <div className="text-content">
               <div className="redBlock">
-                <h2>Welcome to Keansburg Park</h2>
+                <h2>1. Welcome to Keansburg Park</h2>
               </div>
               <br />
               <p>
@@ -85,13 +64,13 @@ const Homepage = () => {
 
         <section id="activities" className="activities">
           <div className="redBlock">
-            <h2>What do we have?</h2>
+            <h2>2. What do we have?</h2>
           </div>
           <br />
           <br />
           <div className="container">
             <div className="left-side">
-              <h1>Amusement</h1>
+              <h2>2.1. Amusement</h2>
             </div>
             <div className="right-side">
               <div className="image-container">
@@ -117,6 +96,37 @@ const Homepage = () => {
               <div className="image-container">
                 <img src={game} alt="Carnival Games" />
                 <div className="caption">Carnival Games</div>
+              </div>
+            </div>
+          </div>
+          <div className="container">
+            <div className="left-side">
+              <h2>2.2. Water park</h2>
+            </div>
+            <div className="right-side">
+              <div className="image-container">
+                <img src={slides} alt="Slides" />
+                <div className="caption">Slides</div>
+              </div>
+              <div className="image-container">
+                <img src={lagoon} alt="Kiddie lagoon" />
+                <div className="caption">Kiddie lagoon</div>
+              </div>
+              <div className="image-container">
+                <img src={tub} alt="Hot tub" />
+                <div className="caption">Hot tub</div>
+              </div>
+              <div className="image-container">
+                <img src={changing} alt="Changing rooms" />
+                <div className="caption">Changing room</div>
+              </div>
+              <div className="image-container">
+                <img src={lockers} alt="Lockers" />
+                <div className="caption">Lockers</div>
+              </div>
+              <div className="image-container">
+                <img src={dining} alt="Dining" />
+                <div className="caption">Dining</div>
               </div>
             </div>
           </div>
